88a39e47e6cb07e3a8ff4a6f1408f2774d30266e
Satya Prakash Nandy <satyaprakashnandy@gmail.com>
2024-06-02 15:33:30 +0530

    Add Color Changes

--- src/component/platform/util/colors.js
+++ src/component/platform/util/colors.js
@@ -5,5 +5,9 @@ const HEXA_COLOR = {
   "8ef4fb": 0x8ef4fb,
   119092: 0x119092,
   bb8fce: 0xbb8fce,
+  "56d28c": 0x56d28c,
+  "1d817a": 0x1d817a,
+  "0c1718": 0x0c1718,
+  "c1e1cf": 0xc1e1cf
 };
 export default HEXA_COLOR;
